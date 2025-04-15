b20b64697f311d5d3624a55b1b6758d026a705dd
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-04-15 10:52:59 +0530

    remove unwanted code lines

--- Backend/src/main/java/com/example/Backend/Model/UserModel.java
+++ Backend/src/main/java/com/example/Backend/Model/UserModel.java
@@ -7,34 +7,16 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
 @Getter
+@Setter
 @Entity
 public class UserModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
+
     private String name;
-    @Setter
-    @Getter
     private String email;
-    @Setter
-    @Getter
     private String password;
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String name() {
-        return name;
-    }
-    public void name(String username) {
-        this.name = username;
-    }
-
-
 }
